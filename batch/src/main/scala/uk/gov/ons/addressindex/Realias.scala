685c4f6b016e583cd1ac6043e3f7bcdb42de219e
ThomasPoot <35296806+ThomasPoot@users.noreply.github.com>
2019-07-17 16:08:08 +0100

    Final feature addition - JSON parsing, proper filtering and regex. Temporarily disabled the actual alias push for debugging.

--- batch/src/main/scala/uk/gov/ons/addressindex/Realias.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/Realias.scala
@@ -3,11 +3,37 @@ package uk.gov.ons.addressindex
 import com.typesafe.config.ConfigFactory
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 import play.api.libs.functional.syntax._
-import play.api.libs.json.Reads._
 import play.api.libs.json._
 import scalaj.http.{Http, HttpResponse}
 
 object Realias extends App {
+
+  val config = ConfigFactory.load()
+
+  val opts = new ScallopConf(args) {
+    banner(
+      """
+Realias script.
+
+Automatically updates all of the ES cluster indexes to match the specified epoch, if it exists.
+
+Example: sbt "realias 65"
+
+For usage see below:
+  """)
+
+    val epoch_opt: ScallopOption[String] = opt(
+      "epoch", descr = "Epoch to update aliases to",
+      noshort = true, required = true)
+    val prefix_opt: ScallopOption[String] = opt(
+      "prefix", descr = "Prefix to place before the index names",
+      default = Some("index"), noshort = true)
+    verify()
+    val epoch: String = epoch_opt.toOption.get
+    val prefix: String = prefix_opt.toOption.get
+  }
+
+  // Class definitions, only used here, mostly for parsing JSON
 
   sealed trait AliasCommand
 
@@ -15,8 +41,11 @@ object Realias extends App {
 
   case class AddAlias(index: String, alias: String) extends AliasCommand
 
+  // The JSON document posted to /_aliases
   case class PostAliases(actions: Seq[AliasCommand])
 
+  // The inner element of the JSON document received from /_aliases/*
+  // We can't describe the entire document as a class, because it uses dynamic keys
   case class IndexAliases(index: String, aliases: Seq[String])
 
   // Classes for the POST to /_aliases
@@ -31,39 +60,29 @@ object Realias extends App {
       (__ \ "add" \ "alias").write[String]
     ) (unlift(AddAlias.unapply))
 
-  implicit val writeAliasCommand: Writes[AliasCommand] = {
-    case _: RemoveAlias => writeRemoveAlias
-    case _: AddAlias => writeAddAlias
+  implicit val writeAliasCommand: Writes[AliasCommand] = new Writes[AliasCommand] {
+    def writes(ac: AliasCommand): JsValue = {
+      ac match {
+        case c: RemoveAlias => writeRemoveAlias.writes(c)
+        case c: AddAlias => writeAddAlias.writes(c)
+      }
+    }
   }
 
-  implicit val writePostAliases: Writes[PostAliases] = (
-    (__ \ "actions").write[Seq[AliasCommand]],
-    ) (unlift(PostAliases.unapply))
+  implicit val writePostAliases: Writes[PostAliases] =
+    (__ \ "actions").write[Seq[AliasCommand]].contramap[PostAliases](_.actions)
 
   // Classes for the GET from /_aliases/*
 
-  implicit val readIndexAliases: Reads[Seq[IndexAliases]] = (j: JsValue) => j.as[JsObject].fields.map(
-    t => IndexAliases(t._1, (t._2 \ "aliases").as[JsObject].keys.toSeq)
-  )
-
-  val config = ConfigFactory.load()
-
-  val opts = new ScallopConf(args) {
-    banner(
-      """
-Realias script.
-
-Automatically updates all of the ES cluster indexes to match the specified epoch, if it exists.
-
-Example: sbt "realias 65"
-
-For usage see below:
-      """)
-
-    val epoch: ScallopOption[String] = opt("epoch", noshort = true, descr = "Epoch to update aliases to")
-    val prefix: ScallopOption[String] = opt("prefix", noshort = true, descr = "Prefix to place before the index names").orElse(Some("index"))
-    verify()
+  implicit val readIndexAliases: Reads[Seq[IndexAliases]] = new Reads[Seq[IndexAliases]] {
+    def reads(j: JsValue): JsResult[Seq[IndexAliases]] = {
+      JsSuccess(j.as[JsObject].fields.map(
+        t => IndexAliases(t._1, (t._2 \ "aliases").as[JsObject].keys.toSeq)
+      ))
+    }
   }
+
+  // Main code start
 
   val nodes = config.getString("addressindex.elasticsearch.nodes")
   val port = config.getString("addressindex.elasticsearch.port")
@@ -75,13 +94,13 @@ For usage see below:
 
   val index_aliases = Json.parse(get_response.body).as[Seq[IndexAliases]]
 
-  val target_indexes_regex = s"^hybrid(-historical)?(-skinny)?_${opts.epoch}_".r
+  val target_indexes_regex = raw"^hybrid(-historical)?(-skinny)?_${opts.epoch}".r
   val target_indexes = index_aliases.filter(i => target_indexes_regex.findFirstIn(i.index).isDefined).map(_.index)
-  println("Current indexes:", target_indexes)
+  println(s"Current indexes: ${target_indexes.mkString(",")}")
 
-  val target_aliases_regex = s"beta_(full|skinny)_(hist|nohist)_(${opts.epoch}|current)".r
+  val target_aliases_regex = raw"^${opts.prefix}_(full|skinny)_(hist|nohist)_(${opts.epoch}|current)".r
   val target_aliases = index_aliases.flatMap(_.aliases).filter(target_aliases_regex.findFirstIn(_).isDefined)
-  println("Current aliases:", target_aliases)
+  println(s"Current aliases: ${target_aliases.mkString(",")}")
 
   // create an alias from an index name
   def make_aliases(index_name: String): Seq[String] = {
@@ -111,9 +130,11 @@ For usage see below:
 
   val data = PostAliases(alias_deletions ++ alias_additions)
 
-  val post_response: HttpResponse[String] = Http(url + "_alias")
-    .postData(Json.toJson[PostAliases](data).toString())
-    .header("Content-type", "application/json")
-    .asString
-  if (post_response.code != 200) throw new Exception(s"Could not create aliases using POST: code ${post_response.code} body ${post_response.body}")
+  println(Json.toJson[PostAliases](data).toString)
+
+  //  val post_response: HttpResponse[String] = Http(url + "_alias")
+  //    .postData(Json.toJson[PostAliases](data).toString())
+  //    .header("Content-type", "application/json")
+  //    .asString
+  //  if (post_response.code != 200) throw new Exception(s"Could not create aliases using POST: code ${post_response.code} body ${post_response.body}")
 }
