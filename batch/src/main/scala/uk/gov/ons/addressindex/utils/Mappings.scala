a027f38c70b3cc9301f43ed8337e301cd27b6a12
Richard Smith <richard.m.smith@ons.gov.uk>
2019-12-12 16:03:00 +0000

    change shards to 1 and skinny bm25 settings to 0.1

--- batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
+++ batch/src/main/scala/uk/gov/ons/addressindex/utils/Mappings.scala
@@ -6,7 +6,7 @@ object Mappings {
     """
       {
           "settings": {
-              "number_of_shards": 4,
+              "number_of_shards": 1,
               "number_of_replicas": 0,
               "index.queries.cache.enabled": false,
               "index": {
@@ -906,15 +906,15 @@ object Mappings {
     """
       {
           "settings": {
-              "number_of_shards": 4,
+              "number_of_shards": 1,
               "number_of_replicas": 0,
               "index.queries.cache.enabled": false,
               "index": {
                   "similarity": {
                       "default": {
                           "type": "BM25",
-                          "b": "0.75",
-                          "k1": "0.3"
+                          "b": "0.1",
+                          "k1": "0.1"
                       }
                   }
               },
